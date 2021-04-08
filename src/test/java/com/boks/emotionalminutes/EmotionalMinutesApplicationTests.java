00ef3b552927bfdaca799019042efe0043c94899
Nam-Ki-Bok <nkb7714@naver.com>
2021-04-08 17:46:34 +0900

    [2021.04.08] First Commit

---
+++ src/test/java/com/boks/emotionalminutes/EmotionalMinutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boks.emotionalminutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionalMinutesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

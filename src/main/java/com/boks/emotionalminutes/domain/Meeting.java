999b6a23985cd2f84e94ffa60cab138db318f548
방규빈 <rbqls1057@gmail.com>
2021-04-16 12:59:15 +0900

    In-Memory Login

---
+++ src/main/java/com/boks/emotionalminutes/domain/Meeting.java
@@ -0,0 +1,27 @@
+package com.boks.emotionalminutes.domain;
+
+import com.boks.emotionalminutes.domain.user.User;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
+
+@Entity
+@Getter
+public class Meeting {
+    @Id
+    private String code;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String name;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @Column(name = "progress_time")
+    private Time progressTime;
+}
---
+++ src/main/java/com/boks/emotionalminutes/domain/user/User.java
@@ -0,0 +1,35 @@
+package com.boks.emotionalminutes.domain.user;
+
+import com.boks.emotionalminutes.domain.Participation;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @Builder
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public User update(String name) {
+        this.name = name;
+
+        return this;
+    }
+}

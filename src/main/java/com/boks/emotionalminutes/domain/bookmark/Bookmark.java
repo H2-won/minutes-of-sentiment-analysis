bff9ca0d5349dd86bd5ff6abbdf22a55f899fa90
Nam-Ki-Bok <nkb7714@naver.com>
2021-04-22 17:09:14 +0900

    [2021.04.22] Bookmark Entity 추가

---
+++ src/main/java/com/boks/emotionalminutes/domain/bookmark/Bookmark.java
@@ -0,0 +1,31 @@
+package com.boks.emotionalminutes.domain.bookmark;
+
+import com.boks.emotionalminutes.domain.sentence.Sentence;
+import com.boks.emotionalminutes.domain.user.User;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class Bookmark {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    // 한명의 유저에 여러개 북마크가 있으니 북마크(Many) to 유저(one)
+    // ManyToOne 이 맞나?
+    // 근데 북마크 내용이 컬럼으로 있으니 전부 OneToOne 으로 해야하는거 아닌가..?
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // 여기도?
+    // 다대다 일대다 다대일...
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sentence_id")
+    private Sentence sentence;
+
+    @Column(name = "memo")
+    private String memo;
+}

72f2a86e2a32e56e022379e59ccd2f18be1e997f
Nam-Ki-Bok <nkb7714@naver.com>
2021-04-22 17:18:21 +0900

    [2021.04.22] IntervalKeywords Entity 추가

---
+++ src/main/java/com/boks/emotionalminutes/domain/intervalKeywords/IntervalKeywords.java
@@ -0,0 +1,33 @@
+package com.boks.emotionalminutes.domain.intervalKeywords;
+
+import com.boks.emotionalminutes.domain.minutes.Minutes;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class IntervalKeywords {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "minutes_id")
+    private Minutes minutes;
+
+    @Column(name = "interval1_keywords")
+    private String interval1Keywords;
+
+    @Column(name = "interval2_keywords")
+    private String interval2Keywords;
+
+    @Column(name = "interval3_keywords")
+    private String interval3Keywords;
+
+    @Column(name = "interval4_keywords")
+    private String interval4Keywords;
+
+    @Column(name = "interval5_keywords")
+    private String interval5Keywords;
+}

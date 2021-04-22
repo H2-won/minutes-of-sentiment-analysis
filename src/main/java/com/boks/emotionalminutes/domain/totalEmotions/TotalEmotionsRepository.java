3e173d19848b101a11620da6e8d8698a1ec42847
Nam-Ki-Bok <nkb7714@naver.com>
2021-04-22 17:28:34 +0900

    [2021.04.22] TotalEmotions Entity 추가

---
+++ src/main/java/com/boks/emotionalminutes/domain/totalEmotions/TotalEmotionsRepository.java
@@ -0,0 +1,6 @@
+package com.boks.emotionalminutes.domain.totalEmotions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TotalEmotionsRepository extends JpaRepository<TotalEmotions, Long> {
+}

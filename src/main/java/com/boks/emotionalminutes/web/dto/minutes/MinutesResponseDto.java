788e0281e151bd34620a913a86dd68ed125fc9d3
Nam-Ki-Bok <nkb7714@naver.com>
2021-04-27 21:53:55 +0900

    [2021.04.27] Response DTO 생성

--- src/main/java/com/boks/emotionalminutes/web/dto/minutes/MinutesResponseDto.java
+++ src/main/java/com/boks/emotionalminutes/web/dto/minutes/MinutesResponseDto.java
@@ -1,6 +1,7 @@
 package com.boks.emotionalminutes.web.dto.minutes;
 
 import com.boks.emotionalminutes.domain.meeting.Meeting;
+import com.boks.emotionalminutes.domain.minutes.Minutes;
 import lombok.Data;
 import lombok.Getter;
 
@@ -11,4 +12,11 @@ public class MinutesResponseDto {
     private final Meeting meeting;
     private final String password;
     private final String voiceFileLink;
+
+    public MinutesResponseDto (Minutes entity) {
+        this.id = entity.getId();
+        this.meeting = getMeeting();
+        this.password = getPassword();
+        this.voiceFileLink = getVoiceFileLink();
+    }
 }

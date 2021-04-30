d6d482e3adb447f17a846dfefbfbb09672a1902d
Nam-Ki-Bok <nkb7714@naver.com>
2021-04-30 15:38:34 +0900

    [2021.04.30] Entity Controller 생성

--- src/main/java/com/boks/emotionalminutes/controller/TotalKeywordsController.java
+++ src/main/java/com/boks/emotionalminutes/controller/TotalKeywordsController.java
@@ -1,9 +1,29 @@
 package com.boks.emotionalminutes.controller;
 
+import com.boks.emotionalminutes.domain.minutes.Minutes;
+import com.boks.emotionalminutes.domain.totalKeywords.TotalKeywords;
+import com.boks.emotionalminutes.service.TotalKeywordsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class TotalKeywordsController {
+    private final TotalKeywordsService totalKeywordsService;
+
+    @PostMapping("/api/totalkeywords")
+    public TotalKeywords save(@RequestParam("minutes") Minutes minutes,
+                              @RequestParam("main_keyword1") String mainKeywords1,
+                              @RequestParam("main_keyword2") String mainKeywords2,
+                              @RequestParam("main_keyword3") String mainKeywords3,
+                              @RequestParam("main_keyword4") String mainKeywords4,
+                              @RequestParam("main_keyword5") String mainKeywords5) {
+
+        return totalKeywordsService.save(minutes, mainKeywords1, mainKeywords2,
+                mainKeywords3, mainKeywords4, mainKeywords5);
+    }
+
+
 }

7eee97fcc5fb8aadf3b6953237353d63d1d21adb
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-01 15:08:41 +0900

    [2021.05.01] Bookmark Entity 저장 과정 변경

--- src/main/java/com/boks/emotionalminutes/controller/BookmarkController.java
+++ src/main/java/com/boks/emotionalminutes/controller/BookmarkController.java
@@ -1,12 +1,11 @@
 package com.boks.emotionalminutes.controller;
 
 import com.boks.emotionalminutes.domain.bookmark.Bookmark;
-import com.boks.emotionalminutes.domain.sentence.Sentence;
-import com.boks.emotionalminutes.domain.user.User;
 import com.boks.emotionalminutes.service.BookmarkService;
+import com.boks.emotionalminutes.web.dto.bookmark.BookmarkRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -15,10 +14,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @PostMapping("/api/bookmark")
-    public Bookmark save(@RequestParam("user_id") User user,
-                         @RequestParam("sentence_id") Sentence sentence,
-                         @RequestParam("memo") String memo) {
-
-        return bookmarkService.save(user, sentence, memo);
+    public Bookmark save(@RequestBody BookmarkRequestDto requestDto) {
+        return bookmarkService.save(requestDto);
     }
 }
--- src/main/java/com/boks/emotionalminutes/service/BookmarkService.java
+++ src/main/java/com/boks/emotionalminutes/service/BookmarkService.java
@@ -2,11 +2,11 @@ package com.boks.emotionalminutes.service;
 
 import com.boks.emotionalminutes.domain.bookmark.Bookmark;
 import com.boks.emotionalminutes.domain.bookmark.BookmarkRepository;
-import com.boks.emotionalminutes.domain.sentence.Sentence;
-import com.boks.emotionalminutes.domain.user.User;
+import com.boks.emotionalminutes.web.dto.bookmark.BookmarkRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -15,14 +15,7 @@ public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
 
     @Transactional
-    public Bookmark save(User user, Sentence sentence, String memo) {
-        Bookmark bookmark = Bookmark.builder()
-                .user(user)
-                .sentence(sentence)
-                .memo(memo)
-                .build();
-
-        bookmarkRepository.save(bookmark);
-        return bookmark;
+    public Bookmark save(@RequestBody BookmarkRequestDto requestDto) {
+        return bookmarkRepository.save(requestDto.toEntity());
     }
 }

07adb50116efef1ef23c1ef1e6b7880e7a423a96
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-02 21:46:35 +0900

    [2021.05.02] TotalKeywords Save 과정 변경

--- src/main/java/com/boks/emotionalminutes/controller/TotalKeywordsController.java
+++ src/main/java/com/boks/emotionalminutes/controller/TotalKeywordsController.java
@@ -1,11 +1,11 @@
 package com.boks.emotionalminutes.controller;
 
-import com.boks.emotionalminutes.domain.minutes.Minutes;
 import com.boks.emotionalminutes.domain.totalKeywords.TotalKeywords;
 import com.boks.emotionalminutes.service.TotalKeywordsService;
+import com.boks.emotionalminutes.web.dto.totalKeywords.TotalKeywordsRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -14,16 +14,7 @@ public class TotalKeywordsController {
     private final TotalKeywordsService totalKeywordsService;
 
     @PostMapping("/api/total-keywords")
-    public TotalKeywords save(@RequestParam("minutes") Minutes minutes,
-                              @RequestParam("main_keyword1") String mainKeywords1,
-                              @RequestParam("main_keyword2") String mainKeywords2,
-                              @RequestParam("main_keyword3") String mainKeywords3,
-                              @RequestParam("main_keyword4") String mainKeywords4,
-                              @RequestParam("main_keyword5") String mainKeywords5) {
-
-        return totalKeywordsService.save(minutes, mainKeywords1, mainKeywords2,
-                mainKeywords3, mainKeywords4, mainKeywords5);
+    public TotalKeywords save(@RequestBody TotalKeywordsRequestDto requestDto) {
+        return totalKeywordsService.save(requestDto);
     }
-
-
 }
--- src/main/java/com/boks/emotionalminutes/service/TotalKeywordsService.java
+++ src/main/java/com/boks/emotionalminutes/service/TotalKeywordsService.java
@@ -1,11 +1,12 @@
 package com.boks.emotionalminutes.service;
 
-import com.boks.emotionalminutes.domain.minutes.Minutes;
 import com.boks.emotionalminutes.domain.totalKeywords.TotalKeywords;
 import com.boks.emotionalminutes.domain.totalKeywords.TotalKeywordsRepository;
+import com.boks.emotionalminutes.web.dto.totalKeywords.TotalKeywordsRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -14,18 +15,7 @@ public class TotalKeywordsService {
     private final TotalKeywordsRepository totalKeywordsRepository;
 
     @Transactional
-    public TotalKeywords save(Minutes minutes, String mainKeyword1, String mainKeyword2, String mainKeyword3,
-                              String mainKeyword4, String mainKeyword5) {
-        TotalKeywords totalKeywords = TotalKeywords.builder()
-                .minutes(minutes)
-                .mainKeyword1(mainKeyword1)
-                .mainKeyword2(mainKeyword2)
-                .mainKeyword3(mainKeyword3)
-                .mainKeyword4(mainKeyword4)
-                .mainKeyword5(mainKeyword5)
-                .build();
-
-        totalKeywordsRepository.save(totalKeywords);
-        return totalKeywords;
+    public TotalKeywords save(@RequestBody TotalKeywordsRequestDto requestDto) {
+        return totalKeywordsRepository.save(requestDto.toEntity());
     }
 }

a48d14de9f01a9497eb8358b3fe00ab3f1c62cd1
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-03 16:23:17 +0900

    [2021.05.03] Sentence Save 추가

--- src/main/java/com/boks/emotionalminutes/controller/SentenceController.java
+++ src/main/java/com/boks/emotionalminutes/controller/SentenceController.java
@@ -1,15 +1,12 @@
 package com.boks.emotionalminutes.controller;
 
-import com.boks.emotionalminutes.domain.minutes.Minutes;
 import com.boks.emotionalminutes.domain.sentence.Sentence;
-import com.boks.emotionalminutes.domain.user.User;
 import com.boks.emotionalminutes.service.SentenceService;
+import com.boks.emotionalminutes.web.dto.sentence.SentenceRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.Time;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,12 +14,7 @@ public class SentenceController {
     private final SentenceService sentenceService;
 
     @PostMapping("/api/sentence")
-    public Sentence save(@RequestParam("user_id") User user,
-                         @RequestParam("minutes_id") Minutes minutes,
-                         @RequestParam("content") String content,
-                         @RequestParam("emotion") String emotion,
-                         @RequestParam("created_time") Time created_time) {
-
-        return sentenceService.save(user, minutes, content, emotion, created_time);
+    public Sentence save(@RequestBody SentenceRequestDto sentenceRequestDto) {
+        return sentenceService.save(sentenceRequestDto);
     }
 }
--- src/main/java/com/boks/emotionalminutes/service/SentenceService.java
+++ src/main/java/com/boks/emotionalminutes/service/SentenceService.java
@@ -1,14 +1,11 @@
 package com.boks.emotionalminutes.service;
 
-import com.boks.emotionalminutes.domain.minutes.Minutes;
 import com.boks.emotionalminutes.domain.sentence.Sentence;
 import com.boks.emotionalminutes.domain.sentence.SentenceRepository;
-import com.boks.emotionalminutes.domain.user.User;
+import com.boks.emotionalminutes.web.dto.sentence.SentenceRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.sql.Time;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -17,16 +14,7 @@ public class SentenceService {
     private final SentenceRepository sentenceRepository;
 
     @Transactional
-    public Sentence save(User user, Minutes minutes, String content, String emotion, Time createdTime) {
-        Sentence sentence = Sentence.builder()
-                .user(user)
-                .minutes(minutes)
-                .content(content)
-                .emotion(emotion)
-                .createdTime(createdTime)
-                .build();
-
-        sentenceRepository.save(sentence);
-        return sentence;
+    public Sentence save(SentenceRequestDto sentenceRequestDto) {
+        return sentenceRepository.save(sentenceRequestDto.toEntity());
     }
 }

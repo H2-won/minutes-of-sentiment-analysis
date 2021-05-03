e1041bdaf70160bf3a4a75a4b41b84e47e659b44
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-03 16:14:10 +0900

    [2021.05.03] 주석 삭제

--- src/main/java/com/boks/emotionalminutes/domain/bookmark/Bookmark.java
+++ src/main/java/com/boks/emotionalminutes/domain/bookmark/Bookmark.java
@@ -17,15 +17,10 @@ public class Bookmark {
     @Column
     private Long id;
 
-    // 한명의 유저에 여러개 북마크가 있으니 북마크(Many) to 유저(one)
-    // ManyToOne 이 맞나?
-    // 근데 북마크 내용이 컬럼으로 있으니 전부 OneToOne 으로 해야하는거 아닌가..?
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    // 여기도?
-    // 다대다 일대다 다대일...
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sentence_id")
     private Sentence sentence;

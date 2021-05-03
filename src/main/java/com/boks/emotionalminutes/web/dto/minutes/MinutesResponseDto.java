08a89dc200830d74a9bf069f350e71a9a86e8496
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-03 16:14:42 +0900

    [2021.05.03] 종속관계 객체 추가

--- src/main/java/com/boks/emotionalminutes/web/dto/minutes/MinutesResponseDto.java
+++ src/main/java/com/boks/emotionalminutes/web/dto/minutes/MinutesResponseDto.java
@@ -1,9 +1,15 @@
 package com.boks.emotionalminutes.web.dto.minutes;
 
+import com.boks.emotionalminutes.domain.intervalKeywords.IntervalKeywords;
 import com.boks.emotionalminutes.domain.meeting.Meeting;
 import com.boks.emotionalminutes.domain.minutes.Minutes;
+import com.boks.emotionalminutes.domain.sentence.Sentence;
+import com.boks.emotionalminutes.domain.totalEmotions.TotalEmotions;
+import com.boks.emotionalminutes.domain.totalKeywords.TotalKeywords;
 import lombok.Data;
 import lombok.Getter;
+
+import java.util.List;
 
 @Data
 @Getter
@@ -13,10 +19,20 @@ public class MinutesResponseDto {
     private final String password;
     private final String voiceFileLink;
 
+    private final List<Sentence> sentenceList;
+    private final IntervalKeywords intervalKeywords;
+    private final TotalEmotions totalEmotions;
+    private final TotalKeywords totalKeywords;
+
     public MinutesResponseDto (Minutes entity) {
         this.id = entity.getId();
         this.meeting = getMeeting();
         this.password = getPassword();
         this.voiceFileLink = getVoiceFileLink();
+
+        this.sentenceList = entity.getSentences();
+        this.intervalKeywords = entity.getIntervalKeywords();
+        this.totalEmotions = entity.getTotalEmotions();
+        this.totalKeywords = entity.getTotalKeywords();
     }
 }

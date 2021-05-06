49db662c1022bc7d61e462f27c89421bff06462a
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-06 16:26:06 +0900

    [2021.05.06] Sentence 주석 삭제

--- src/main/java/com/boks/emotionalminutes/domain/sentence/Sentence.java
+++ src/main/java/com/boks/emotionalminutes/domain/sentence/Sentence.java
@@ -4,19 +4,20 @@ import com.boks.emotionalminutes.domain.minutes.Minutes;
 import com.boks.emotionalminutes.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.sql.Time;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Sentence {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Long id;
 
-    // 이게 맞나?
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;

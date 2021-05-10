ce01eaf28e5957d5fec2a4cde32c0504c34a59a3
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-10 18:47:09 +0900

    [2021.05.10] Sentence Save 로직 변경

--- src/main/java/com/boks/emotionalminutes/web/dto/sentence/SentenceRequestDto.java
+++ src/main/java/com/boks/emotionalminutes/web/dto/sentence/SentenceRequestDto.java
@@ -1,10 +1,8 @@
 package com.boks.emotionalminutes.web.dto.sentence;
 
 import com.boks.emotionalminutes.domain.minutes.Minutes;
-import com.boks.emotionalminutes.domain.minutes.MinutesRepository;
 import com.boks.emotionalminutes.domain.sentence.Sentence;
 import com.boks.emotionalminutes.domain.user.User;
-import com.boks.emotionalminutes.domain.user.UserRepository;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,19 +11,13 @@ import java.sql.Time;
 @Getter
 @NoArgsConstructor
 public class SentenceRequestDto {
-    private UserRepository userRepository;
-    private MinutesRepository minutesRepository;
-
     private Long userId;
     private Long minutesId;
     private String content;
     private String emotion;
     private Time createdTime;
 
-    public Sentence toEntity() {
-        User user = userRepository.findById(userId).get();
-        Minutes minutes = minutesRepository.findById(minutesId).get();
-
+    public Sentence toEntity(User user, Minutes minutes) {
         return Sentence.builder()
                 .user(user)
                 .minutes(minutes)

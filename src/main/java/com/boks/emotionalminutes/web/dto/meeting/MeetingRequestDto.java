bb956a54cfc1db13c5a5c7ef14e7c1a7b058256c
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-10 16:21:01 +0900

    [2021.05.10] Request DTO 자료형 수정

--- src/main/java/com/boks/emotionalminutes/web/dto/meeting/MeetingRequestDto.java
+++ src/main/java/com/boks/emotionalminutes/web/dto/meeting/MeetingRequestDto.java
@@ -2,7 +2,6 @@ package com.boks.emotionalminutes.web.dto.meeting;
 
 import com.boks.emotionalminutes.domain.meeting.Meeting;
 import com.boks.emotionalminutes.domain.user.User;
-import com.boks.emotionalminutes.domain.user.UserRepository;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,15 +10,11 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class MeetingRequestDto {
-    private UserRepository userRepository;
-
     private String code;
     private Long userId;
     private String name;
 
-    public Meeting toEntity() {
-        User user = userRepository.findById(userId).get();
-
+    public Meeting toEntity(User user) {
         return Meeting.builder()
                 .code(code)
                 .user(user)

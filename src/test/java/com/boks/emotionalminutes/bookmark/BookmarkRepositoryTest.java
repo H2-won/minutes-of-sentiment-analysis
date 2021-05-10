052f3c899494b1cdfe7b5680cda29d80368c4d98
방규빈 <rbqls1057@gmail.com>
2021-05-10 17:40:42 +0900

    start test

---
+++ src/test/java/com/boks/emotionalminutes/bookmark/BookmarkRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.boks.emotionalminutes.bookmark;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BookmarkRepositoryTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+}

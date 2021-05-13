b86763949b0d841b5b75ac0b5a6eb86dc5a34e49
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-13 17:22:16 +0900

    [2021.05.13] TotalKeywords Response DTO id 필드 삭제

--- src/main/java/com/boks/emotionalminutes/web/dto/totalKeywords/TotalKeywordsResponseDto.java
+++ src/main/java/com/boks/emotionalminutes/web/dto/totalKeywords/TotalKeywordsResponseDto.java
@@ -1,6 +1,5 @@
 package com.boks.emotionalminutes.web.dto.totalKeywords;
 
-import com.boks.emotionalminutes.domain.minutes.Minutes;
 import com.boks.emotionalminutes.domain.totalKeywords.TotalKeywords;
 import lombok.Data;
 import lombok.Getter;
@@ -8,7 +7,6 @@ import lombok.Getter;
 @Data
 @Getter
 public class TotalKeywordsResponseDto {
-    private Long id;
     private String mainKeyword1;
     private String mainKeyword2;
     private String mainKeyword3;
@@ -16,7 +14,6 @@ public class TotalKeywordsResponseDto {
     private String mainKeyword5;
 
     public TotalKeywordsResponseDto (TotalKeywords entity) {
-        this.id = entity.getId();
         this.mainKeyword1 = entity.getMainKeyword1();
         this.mainKeyword2 = entity.getMainKeyword2();
         this.mainKeyword3 = entity.getMainKeyword3();

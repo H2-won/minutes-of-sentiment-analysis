719c7126f6b8ab8cb27980fbac8bf3ef5abbb8ac
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-13 14:56:12 +0900

    [2021.05.13] array import 삭제

--- src/main/java/com/boks/emotionalminutes/domain/sentence/SentenceRepository.java
+++ src/main/java/com/boks/emotionalminutes/domain/sentence/SentenceRepository.java
@@ -3,8 +3,6 @@ package com.boks.emotionalminutes.domain.sentence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface SentenceRepository extends JpaRepository<Sentence, Long> {
 }

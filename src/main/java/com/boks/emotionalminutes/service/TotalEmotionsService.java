f5584ab93156cc9fe3820f3b1ae4d693afea7641
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-13 21:47:24 +0900

    [2021.05.13] TotalEmotions 생성 서비스 로직 추가

--- src/main/java/com/boks/emotionalminutes/service/TotalEmotionsService.java
+++ src/main/java/com/boks/emotionalminutes/service/TotalEmotionsService.java
@@ -2,12 +2,16 @@ package com.boks.emotionalminutes.service;
 
 import com.boks.emotionalminutes.domain.minutes.Minutes;
 import com.boks.emotionalminutes.domain.minutes.MinutesRepository;
+import com.boks.emotionalminutes.domain.sentence.Sentence;
+import com.boks.emotionalminutes.domain.totalEmotions.TotalEmotions;
 import com.boks.emotionalminutes.domain.totalEmotions.TotalEmotionsRepository;
 import com.boks.emotionalminutes.web.dto.totalEmotions.TotalEmotionsRequestDto;
 import com.boks.emotionalminutes.web.dto.totalEmotions.TotalEmotionsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -19,7 +23,34 @@ public class TotalEmotionsService {
     @Transactional
     public Long save(TotalEmotionsRequestDto requestDto) {
         Minutes minutes = minutesRepository.findById(requestDto.getMinutesId()).get();
-        return totalEmotionsRepository.save(requestDto.toEntity(minutes)).getId();
+
+        List<Sentence> sentences = minutes.getSentences();
+        float total = sentences.size();
+        float happy = 0;
+        float emotionless = 0;
+        float angry = 0;
+        float sad = 0;
+
+        for(Sentence value : sentences) {
+            String emotion = value.getEmotion();
+            if ("기쁨".equals(emotion)) {
+                happy += 1;
+            } else if ("무감정".equals(emotion)) {
+                emotionless += 1;
+            } else if ("화남".equals(emotion)) {
+                angry += 1;
+            } else if ("슬픔".equals(emotion)) {
+                sad += 1;
+            }
+        }
+
+        TotalEmotions entity = TotalEmotions.builder()
+                .happy(happy / total * 100)
+                .emotionless(emotionless / total * 100)
+                .angry(angry / total * 100)
+                .sad(sad / total * 100)
+                .build();
+        return totalEmotionsRepository.save(entity).getId();
     }
 
     public TotalEmotionsResponseDto findByMinutesId(Long id) {

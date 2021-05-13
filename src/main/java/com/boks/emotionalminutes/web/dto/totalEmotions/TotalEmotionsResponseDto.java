de99a735f39a7eaffff6dedb7d3eadce4428c3ec
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-13 17:21:55 +0900

    [2021.05.13] TotalEmotions Response DTO id 필드 삭제

--- src/main/java/com/boks/emotionalminutes/web/dto/totalEmotions/TotalEmotionsResponseDto.java
+++ src/main/java/com/boks/emotionalminutes/web/dto/totalEmotions/TotalEmotionsResponseDto.java
@@ -1,6 +1,5 @@
 package com.boks.emotionalminutes.web.dto.totalEmotions;
 
-import com.boks.emotionalminutes.domain.minutes.Minutes;
 import com.boks.emotionalminutes.domain.totalEmotions.TotalEmotions;
 import lombok.Data;
 import lombok.Getter;
@@ -8,14 +7,12 @@ import lombok.Getter;
 @Data
 @Getter
 public class TotalEmotionsResponseDto {
-    private Long id;
     private float happy;
     private float emotionless;
     private float sad;
     private float angry;
 
     public TotalEmotionsResponseDto (TotalEmotions entity) {
-        this.id = entity.getId();
         this.happy = entity.getHappy();
         this.emotionless = entity.getEmotionless();
         this.sad = entity.getSad();

ec9fe6901b1c161bacd44f7fad50e78563484aba
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-05-22 16:46:49 +0900

    [2021.05.22] api 절대경로

--- src/main/client/src/controllers/auth.js
+++ src/main/client/src/controllers/auth.js
@@ -1,9 +1,12 @@
 export const getUserInfo = (token, setLogged, setUserInfo) => {
-  fetch('/api/user-info', {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-    Authorization: 'Bearer ' + token,
-  })
+  fetch(
+    'http://ec2-3-34-172-246.ap-northeast-2.compute.amazonaws.com:8080/api/user-info',
+    {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      Authorization: 'Bearer ' + token,
+    },
+  )
     .then((res) => {
       setLogged(true);
       setUserInfo(res);

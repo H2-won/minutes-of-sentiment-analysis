076dccd43a57e54af05a6866889dc85f35ba506f
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-24 15:55:08 +0900

    [2021.05.24] TOKEN 에러 해결

--- src/main/client/src/controllers/meeting.js
+++ src/main/client/src/controllers/meeting.js
@@ -2,7 +2,7 @@ export const produceConference = (title, pw, setConferenceCode, connection) => {
   fetch('/api/meeting', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + localStorage.getItem('accessToken')},
+    Authorization: 'Bearer ' + localStorage.getItem('accessToken')},
     body: JSON.stringify({
       userId: localStorage.getItem('userId'),
       code: '',

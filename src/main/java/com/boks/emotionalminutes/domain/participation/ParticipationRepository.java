9b694f75247845c6e098bba3c972448f6a0d4106
방규빈 <rbqls1057@gmail.com>
2021-05-24 19:22:12 +0900

    meeting Duplication join 처리

--- src/main/java/com/boks/emotionalminutes/domain/participation/ParticipationRepository.java
+++ src/main/java/com/boks/emotionalminutes/domain/participation/ParticipationRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
     List<Participation> findAllByUserId(Long user_id);
+
+    Optional<Participation> findByUserIdAndMeetingCode(Long user_id, String meeting_code);
 }

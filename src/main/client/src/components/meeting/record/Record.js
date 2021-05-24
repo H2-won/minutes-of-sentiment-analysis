0cb6d1728a27e92d89de5307c70a7814e780671d
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-24 15:49:02 +0900

    [2021.05.24] API 토큰 추가

--- src/main/client/src/components/meeting/record/Record.js
+++ src/main/client/src/components/meeting/record/Record.js
@@ -71,7 +71,7 @@ function Record() {
     const userName = localStorage.getItem('userName');
     fetch('/api/sentence', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json', 'Authorization': localStorage.getItem('accessToken') },
+      headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem('accessToken') },
       body: JSON.stringify({
         userId: userId,
         minutesId: userName,

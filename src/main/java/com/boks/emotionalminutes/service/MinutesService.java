a4b6b6b8a383631f659c4c563c5cc816f2938e11
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-25 18:17:14 +0900

    [2021.05.25] API TEST

--- src/main/java/com/boks/emotionalminutes/service/MinutesService.java
+++ src/main/java/com/boks/emotionalminutes/service/MinutesService.java
@@ -6,6 +6,8 @@ import com.boks.emotionalminutes.domain.minutes.Minutes;
 import com.boks.emotionalminutes.domain.minutes.MinutesRepository;
 import com.boks.emotionalminutes.domain.participation.Participation;
 import com.boks.emotionalminutes.domain.sentence.Sentence;
+import com.boks.emotionalminutes.domain.totalEmotions.TotalEmotions;
+import com.boks.emotionalminutes.domain.totalEmotions.TotalEmotionsRepository;
 import com.boks.emotionalminutes.domain.user.User;
 import com.boks.emotionalminutes.domain.user.UserRepository;
 import com.boks.emotionalminutes.web.dto.minutes.MinutesListResponseDto;
@@ -34,6 +36,9 @@ public class MinutesService {
 
     @Autowired
     private final UserRepository userRepository;
+
+    @Autowired
+    private final TotalEmotionsRepository totalEmotionsRepository;
 
     @Transactional
     public Long save(MinutesRequestDto requestDto) {
@@ -102,9 +107,39 @@ public class MinutesService {
         LocalTime progressTime = LocalTime.of(hour, min, sec);
         minutes.update(progressTime);
 
-        TotalEmotionsService totalEmotionsService = null;
-        assert false;
-        totalEmotionsService.save(1L);
+        List<Sentence> sentences = minutes.getSentences();
+        System.out.println("회의록(id = " + id + ") 의 문장 리스트를 가져왔습니다.");
+        System.out.println("발화를 담은 리스트의 길이는 " + sentences.size() + " 입니다.");
+        for (Sentence value : sentences) {
+            System.out.println(value.getContent());
+        }
+        float total = sentences.size();
+        float happy = 0;
+        float emotionless = 0;
+        float angry = 0;
+        float sad = 0;
+
+        for(Sentence value : sentences) {
+            String emotion = value.getEmotion();
+            if ("행복".equals(emotion)) {
+                happy += 1;
+            } else if ("중립".equals(emotion)) {
+                emotionless += 1;
+            } else if ("분노".equals(emotion)) {
+                angry += 1;
+            } else if ("슬픔".equals(emotion)) {
+                sad += 1;
+            }
+        }
+
+        TotalEmotions entity = TotalEmotions.builder()
+                .happy(happy / total * 100)
+                .emotionless(emotionless / total * 100)
+                .angry(angry / total * 100)
+                .sad(sad / total * 100)
+                .build();
+        totalEmotionsRepository.save(entity);
+
         return id;
     }
 }

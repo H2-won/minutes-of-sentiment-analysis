bba97ad168777e72995060746c6c1eda1418deba
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-28 01:43:49 +0900

    [2021.05.28] 중괄호 삽입

--- src/main/java/com/boks/emotionalminutes/controller/MeetingController.java
+++ src/main/java/com/boks/emotionalminutes/controller/MeetingController.java
@@ -18,6 +18,7 @@ public class MeetingController {
     @PostMapping("/api/meeting/join")
     public MeetingJoinResponseDto join(@RequestBody MeetingJoinRequestDto requestDto) {
         return meetingService.join(requestDto);
+    }
 
     @GetMapping("/api/meeting/{code}")
     public MeetingResponseDto findByCode(@PathVariable String code) {

5337a6a6f8d8e4bf7b5d0d75afa894786283cf4f
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-28 00:33:57 +0900

    [2021.05.28] Time 원복

--- src/main/client/src/components/meeting/VideoContainer.js
+++ src/main/client/src/components/meeting/VideoContainer.js
@@ -358,7 +358,8 @@ const VideoContainer = ({ match }) => {
         senderId: userId,
         senderName: userName,
         message: finalTranscript + '.',
-        time: ('0' + hours).slice(-2) + ":" + ('0' + minutes).slice(-2) + ":" +('0' + seconds).slice(-2)
+        // time: ('0' + hours).slice(-2) + ":" + ('0' + minutes).slice(-2) + ":" +('0' + seconds).slice(-2)
+        time: ('0' + now.getHours()).slice(-2) + ":" + ('0' + now.getMinutes()).slice(-2) + ":" +('0' + now.getSeconds()).slice(-2)
       });
     }
   }, [finalTranscript, resetTranscript]);

3898f30219ca493627bde48e155218ef04e526bb
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-05-29 05:20:47 +0900

    [2021.05.29] 회의록 API

--- src/main/client/src/controllers/meetingLog.js
+++ src/main/client/src/controllers/meetingLog.js
@@ -15,3 +15,41 @@ export const getMinutesList = (userId) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const lookupMeetingLogByCode = (code, pw) => {
+  const token = localStorage.getItem('accessToken');
+
+  fetch(`/api/minutes/${code}`, {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + token,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      password: pw,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log('회의록 정보 : ', res);
+      return res;
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getMeetingLogInfo = (minutesId) => {
+  const token = localStorage.getItem('accessToken');
+  fetch(`/api/minutes/${minutesId}`, {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + token,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log('회의록 정보:', res);
+      return res;
+    })
+    .catch((err) => console.log(err));
+};

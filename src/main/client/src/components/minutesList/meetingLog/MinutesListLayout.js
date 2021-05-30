3258f736cc9832aa99db8766fecf53b1823fdd9f
Nam-Ki-Bok <nkb7714@naver.com>
2021-05-30 16:52:17 +0900

    [2021.05.30]

--- src/main/client/src/components/minutesList/meetingLog/MinutesListLayout.js
+++ src/main/client/src/components/minutesList/meetingLog/MinutesListLayout.js
@@ -20,10 +20,27 @@ const Container = styled.div`
 function MinutesListLayout() {
   const [minutesList, setMinutesList] = useState([]);
   let test;
+
   useEffect(() => {
-    test = getMinutesList(localStorage.getItem('userId')) || [];
-    console.log(test.json());
-    setMinutesList(test);
+    // test = getMinutesList(localStorage.getItem('userId')) || [];
+    const userId = localStorage.getItem('userId');
+    const token = localStorage.getItem('accessToken');
+    fetch(`/api/user/${userId}/minutes`, {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-Type': 'application/json',
+      },
+    })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log('회의록 리스트 : ', res);
+          setMinutesList(res);
+        })
+        .catch((err) => {
+          console.log(err);
+          return [];
+        });
   }, []);
   // const meetingLogs = [
   //   {

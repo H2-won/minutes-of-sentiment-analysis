acf745cd492b75a7ad94cdedcc85cb0faddb51f3
방규빈 <rbqls1057@gmail.com>
2021-05-31 15:54:01 +0900

    join response

--- src/main/java/com/boks/emotionalminutes/web/dto/meeting/MeetingJoinResponseDto.java
+++ src/main/java/com/boks/emotionalminutes/web/dto/meeting/MeetingJoinResponseDto.java
@@ -3,8 +3,8 @@ package com.boks.emotionalminutes.web.dto.meeting;
 import com.boks.emotionalminutes.domain.meeting.Meeting;
 
 public class MeetingJoinResponseDto {
-    String meetingCode;
-    Long minutesId;
+    private final String meetingCode;
+    private final Long minutesId;
 
     public MeetingJoinResponseDto (Meeting entity) {
         this.meetingCode = entity.getCode();

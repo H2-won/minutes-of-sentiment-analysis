21db6a1440e4ed2442fe728513ff943d65b0f606
LIMHUIWON <59960678+H2-won@users.noreply.github.com>
2021-06-02 20:10:20 +0900

    [2021.06.02] 음소거 해제 시 recorder 시작

--- src/main/client/src/modules/index.js
+++ src/main/client/src/modules/index.js
@@ -3,12 +3,14 @@ import modal from './modal';
 import meeting from './meeting';
 import meetingLog from './meetingLog';
 import createdDate from './createdDate';
+import connectionInfo from './connectionInfo';
 
 const rootReducer = combineReducers({
   modal,
   meeting,
   meetingLog,
   createdDate,
+  connectionInfo,
 });
 
 export default rootReducer;
